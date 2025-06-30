98705ad03fd2d32045cbf5ea741eee9c6b293c38
Marin <mikleusevicmarin@gmail.com>
2025-06-30 21:17:02 +0200

    More homework

--- Assets/Scripts/Zadaca1.cs
+++ Assets/Scripts/Zadaca1.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class Zadaca1 : MonoBehaviour
 {
     [SerializeField] private GameObject powerUpPrefab;
+    
+    private GameObject powerUpGO;
+    
     private bool isPoweredUp;
     void Update()
     {
@@ -17,11 +20,18 @@ public class Zadaca1 : MonoBehaviour
     {
         isPoweredUp = true;
         
-        GameObject powerUp = Instantiate(powerUpPrefab,
-            new Vector3(transform.position.x, transform.position.y + 3, transform.position.z), Quaternion.identity);
+        if (!powerUpGO)
+        {
+            powerUpGO = Instantiate(powerUpPrefab,
+                new Vector3(transform.position.x, transform.position.y + 3, transform.position.z), Quaternion.identity);
+        }
+        else
+        {
+            powerUpGO.SetActive(true);
+        }
         
         yield return new WaitForSeconds(5);
-        Destroy(powerUp);
+        powerUpGO.SetActive(false);
         
         isPoweredUp = false;
     }
